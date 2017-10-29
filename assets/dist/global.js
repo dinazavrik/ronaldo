5520d12628f66189054e4ca92f1045183e0f1194
Unknown <avedenko.a@mail.ru>
2017-10-29 19:28:05 +0200

    My Project

---
+++ assets/dist/global.js
@@ -0,0 +1,18 @@
+(function ($) {
+
+    $(document).ready(function () {
+
+        $ ( ".img-btn__image" ).interactive_bg ({
+            strength: 15,
+            scale: 1,
+            animationSpeed: "100ms",
+            contain: true,
+            wrapContent: false
+        });
+
+    });
+
+})(jQuery);
+
+
+
